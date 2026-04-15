309e5e876c7c656db2532068caa9e35312a354f8
Wesley2117 <wesleysant741@gmail.com>
2026-04-14 21:27:10 -0300

    feat: Aprendendo  a usar o launchImageLibraryAsync na pratica, e que bom entender como funciona, vejo que nao e mais problema usar funcionalidade que precise de fotos ou videos

--- devFront/AppCamera/App.js
+++ devFront/AppCamera/App.js
@@ -1,21 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, SafeAreaViewBase, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { launchImageLibrary } from 'react-native-image-picker';
+
 
 export default function App() {
+
+  const [photo, setPhoto] = useState(null);
+
+  async function openAlbum(){
+
+    const permission = await ImagePicker.requestCameraPermissionsAsync();
+    if (!permission.granted) {
+        alert("Permissão necessária!");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      quality: 1,
+      allowsMultipleSelection: true,
+      selectionLimit: 5,
+    });
+
+    if (result.canceled) {
+      console.log("Cancelado");
+      return;
+    }
+
+    console.log(result.assets);
+    setPhoto(result.assets[0].uri)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Seja bem vindo ao AppCamera</Text>
-      <Text>Todos os tipos de cameras que voce precisa em um so lugar</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.buttons}>
+        <TouchableOpacity 
+        style={styles.button}
+        onPress={openAlbum}
+        >
+          <Text style={styles.text}>Abrir album</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Abrir camera</Text>
+        </TouchableOpacity>
+      </View>
+
+      {photo !== null && (
+        <Image 
+          source={{ uri: photo}}
+          style={styles.image}
+        />
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    alignItems: "center",
   },
+
+  buttons: {
+    marginTop: 44,
+    flexDirection: "row", 
+    gap: 14,
+    marginBottom: 30
+  }, 
+
+  button: {
+    backgroundColor: '#121212', 
+    padding: 4, 
+    paddingLeft: 16, 
+    paddingRight: 16, 
+    borderRadius: 4,
+  }, 
+
+  text: {
+    color: '#fff'
+  }, 
+
+  image: {
+    width: '90%', 
+    height: 300, 
+    objectFit: 'cover'
+  }
 });
